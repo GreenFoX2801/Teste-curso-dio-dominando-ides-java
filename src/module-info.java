6fb60b415f35431892ae01ea24e072dd4575b5d8
GreenFoX2801 <leoferreirarocha@gmail.com>
2022-11-04 05:28:26 -0300

    Primeiro commit

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author leofe
+ *
+ */
+module primeiroProgramaJava {
+}
---
+++ src/primeiroProgramaJava/PrimeiroPrograma.java
@@ -0,0 +1,13 @@
+package primeiroProgramaJava;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+int a = 2;
+int b = 3;
+System.out.print("A soma de a + b = " + (a+b));
+	}
+
+}
